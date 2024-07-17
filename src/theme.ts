006b4c18c54c7761b0d9e2c4d6929bb9cfaa385f
babchulo1048 <eyoeldemis1048@gmail.com>
2024-07-17 11:01:54 +0300

    Customize the theme to get darker gray

--- src/theme.ts
+++ src/theme.ts
@@ -1,15 +1,29 @@
-// theme.js
-
-// 1. import `extendTheme` function
+// 1. Import `extendTheme` and `ThemeConfig` from Chakra UI
 import { extendTheme, ThemeConfig } from "@chakra-ui/react";
 
-// 2. Add your color mode config
+// 2. Define your color mode configuration
 const config: ThemeConfig = {
   initialColorMode: "dark",
-  //   useSystemColorMode: false,
+  useSystemColorMode: false,
 };
 
-// 3. extend the theme
-const theme = extendTheme({ config });
+// 3. Extend the theme with custom configurations
+const theme = extendTheme({
+  config,
+  colors: {
+    gray: {
+      50: "#f9f9f9",
+      100: "#ededed",
+      200: "#d3d3d3",
+      300: "#b3b3b3",
+      400: "#a0a0a0",
+      500: "#898989",
+      600: "#6c6c6c",
+      700: "#202020",
+      800: "#121212",
+      900: "#111",
+    },
+  },
+});
 
 export default theme;
